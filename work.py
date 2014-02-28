410e42faddf66950203d7dd415e2f6eebdb3b13e
?ngel ?lvarez <angel@nan-tic.com>
2014-02-28 18:53:54 +0100

    fix get_invoice_line function and add invoice_method and pary on list view

--- work.py
+++ work.py
@@ -21,11 +21,24 @@ class Work:
             del cls.project_invoice_method.states['invisible']
 
     def get_invoice_method(self, name):
-        if self.type == 'project':
+        res = super(Work, self).get_invoice_method(name)
+        if self.project_invoice_method:
             return self.project_invoice_method
-        elif self.project_invoice_method:
-            return self.project_invoice_method
-        elif self.parent:
-            return self.parent.invoice_method
-        else:
-            return 'manual'
+        return res
+
+    def _get_lines_to_invoice(self, test=None):
+        "Return lines for work and children"
+        lines = []
+        if test is None:
+            test = self._test_group_invoice()
+        lines += getattr(self, '_get_lines_to_invoice_%s' %
+            self.invoice_method)()
+
+        for children in self.children:
+            if children.type == 'project':
+                if test != children._test_group_invoice():
+                    continue
+            lines += children._get_lines_to_invoice(test=test)
+        return lines
+
+
